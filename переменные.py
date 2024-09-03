60fb3418c225853290628b790b6d5e22c1d20717
Alena <89519703610alena@gmail.com>
2024-09-03 20:36:01 +0500

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,6 @@
+first_name = 'Alena'
+total_tasks = 12
+hours_spent = 1.5
+course = 'python'
+average_bp = hours_spent / total_tasks
+print (first_name,course, total_tasks, hours_spent, average_bp )
